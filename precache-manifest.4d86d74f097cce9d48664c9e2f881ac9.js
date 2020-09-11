21cab84b5b2abe3ce1600ead9f0a88a51603cc20
kalllllor <karolgren3@gmail.com>
2020-09-12 00:41:57 +0200

    Updates

---
+++ precache-manifest.4d86d74f097cce9d48664c9e2f881ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85cbe20b2fd521c4a64c20b44a4b1ed",
+    "url": "/KarolGren/index.html"
+  },
+  {
+    "revision": "dd886086dbcca36536b1",
+    "url": "/KarolGren/static/css/main.f6a000cd.chunk.css"
+  },
+  {
+    "revision": "a82db95f245d56497853",
+    "url": "/KarolGren/static/js/2.7ad6590a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/KarolGren/static/js/2.7ad6590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd886086dbcca36536b1",
+    "url": "/KarolGren/static/js/main.f5aecc02.chunk.js"
+  },
+  {
+    "revision": "038733659f11ed8cb57a",
+    "url": "/KarolGren/static/js/runtime-main.9e812a01.js"
+  }
+]);
